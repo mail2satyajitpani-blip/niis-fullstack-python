9ddf7f77e5564e6dd70ae5263976b7f2c2ad42a6
satyajitpani <mail2satyajitpani@gmail.com>
2026-02-27 10:06:52 +0530

    function

---
+++ python/function/add.py
@@ -0,0 +1,5 @@
+def add():
+   n1=int (input())
+   n2=int (input())
+   print ("SUM=",n1+n2)
+add()
---
+++ python/function/evenodd.py
@@ -0,0 +1,8 @@
+def check():
+   print("Enter a number")
+   n=int(input())
+   if n%2==0:
+   	print("Even")
+   else:
+   	print("Odd")
+check()
