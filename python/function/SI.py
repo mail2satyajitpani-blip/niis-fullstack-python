ab684573d6529823cc39344c7e98c928528e84c5
satyajitpani <mail2satyajitpani@gmail.com>
2026-02-27 10:35:30 +0530

    function program

---
+++ python/function/SI.py
@@ -0,0 +1,10 @@
+def sical():
+	print("Enter principal")
+	p=float(input())
+	print("Enter time")
+	t=float(input())
+	print("Enter rate of interest")
+	r=float(input())
+	SI=(p*t*r)//100
+	print("Simple interest=",SI)
+sical()
